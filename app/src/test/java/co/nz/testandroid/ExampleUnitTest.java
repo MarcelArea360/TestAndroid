8729bc947aefa431cfc7010aa228e8c99f26ac3b
Marcel Braghetto <marcel.braghetto@area360.com>
2015-11-18 15:14:19 +1300

    Testing robolectric in Travis.

--- app/src/test/java/co/nz/testandroid/ExampleUnitTest.java
+++ app/src/test/java/co/nz/testandroid/ExampleUnitTest.java
@@ -1,12 +1,17 @@
 package co.nz.testandroid;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(sdk = RobolectricTestConfig.ROBOLECTRIC_SDK, packageName = RobolectricTestConfig.PACKAGE_NAME, constants = BuildConfig.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
---
+++ app/src/test/java/co/nz/testandroid/RobolectricTestConfig.java
@@ -0,0 +1,10 @@
+package co.nz.testandroid;
+
+/**
+ * Created by Marcel Braghetto on 18/11/15.
+ */
+@SuppressWarnings("unused")
+public class RobolectricTestConfig {
+    public static final int ROBOLECTRIC_SDK = 21;
+    public static final String PACKAGE_NAME = "co.nz.testandroid";
+}
